08f01300709e441a6db82c816d58079a6ab0271d
admin <contact@mabel.li>
2018-12-18 09:12:22 +0000

    Implementing dictionaries to develop calories feature

--- ch05_pizzashop/dict_practice_functionsv2.py
+++ ch05_pizzashop/dict_practice_functionsv2.py
@@ -1,8 +1,19 @@
 toppings = {
     '1':['Pepperoni', 110],
     '2':['Pineapple', 50],
-    '3':['Extra Cheese', 140],
-    '4':['Olives', 55]
+    '4':['Olives', 55],
+    '3':['Extra Cheese', 140]
 }
 
+toppings_list = list(toppings.items())
 
+toppings2 = sorted(toppings_list, key = lambda v : v[1][0])
+
+#print(toppings.values())
+
+for key in toppings:
+    print(key)
+#     toppings.pop(key = lambda v : v[1][1])
+    for value in key:
+        print(value)
+    

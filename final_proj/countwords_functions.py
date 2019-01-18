bd48da5974a836bd163d7bcddbd24f2eb76ef747
admin <contact@mabel.li>
2019-01-18 11:36:42 +0000

    functions for analysing occurrences of words and retrieves top 20 words complete with main function calling each

--- final_proj/countwords_functions.py
+++ final_proj/countwords_functions.py
@@ -2,6 +2,7 @@
 
 # counts = {}
 
+# counts num of occurrences of words into dict
 def count_words(input_txt):
     counts = {}
     for sentence in input_txt:
@@ -17,7 +18,7 @@ def count_words(input_txt):
     # print(counts)
     return counts
 
-# Below calls above function to get counts dict
+# retrieves top 20 of a dict
 def print_top20(counts):
     # counts = count_words(input_txt)
     counts_sorted = sorted(counts.items(), key=lambda kv:kv[1], reverse=True)
--- final_proj/countwords_test.py
+++ final_proj/countwords_test.py
@@ -5,9 +5,8 @@ def main(input_file):
     path = 'text_data/{}'.format(input_file)
     input_txt = open(path)
 
-    # calling funcs 
+    # calling funcs
     counts = count_words(input_txt)
-    # calls func that'll retrieve top 20 of a dict
     print_top20(counts)
 
 # when function called, name of txt file needs to be given as arg
